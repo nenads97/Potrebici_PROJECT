3f34490c8284c6f4674314f1b472ebbed6198852
nsuknovic <smartdeveloper4@outlook.com>
2026-05-06 16:00:42 +0200

    build fix

--- mim-invest-frontend/src/main.tsx
+++ mim-invest-frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "@/shared/styles/global.scss";
+import "./assets/scss/base/_globals.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
